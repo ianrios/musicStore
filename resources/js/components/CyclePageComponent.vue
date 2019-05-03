4728c564f8e8096f1098e39598a0be2cc07efa1a
ianrios <ian.rios1a@gmail.com>
2019-05-03 18:22:57 -0400

    added grief

--- resources/js/components/CyclePageComponent.vue
+++ resources/js/components/CyclePageComponent.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="container" id="cycle_page">
         <h1 id="cycle_page_header1">CYCLE</h1>
+        <!-- <div id="cycle_inderimage"></div> -->
         <img id="bg_img_cycle" src="img/final_album_view_cycle001.png">
+
         <p id="cycle_page_p">{{releases[9].info_p}}</p>
+        <p id="cycle_info"> The first release on the series is presented by <a href="artists#whythough">whythough?</a></p>
+		<p id="cycle_info_2">With raw synths, intense samples, and highly syncopated drums, cycle001: 'grief' is a 16 song, hour long sonic soundscape journey.</p>
+        <p id="release date">Release Date: May 17th, 2019</p>
+        <p id="cycle_info_3">return to this page soon for more release info</p>
     </div>
 </template>
 
